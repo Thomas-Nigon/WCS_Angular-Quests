ce83c7f2ba8a3061a90507ef6ed8f8d0101d39d9
thomas <nigont@gmail.com>
2024-05-07 15:54:12 +0200

    add Angular Q04 router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule, RouterLink } from '@angular/router';
 import { BlockComponent } from './block/block.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { DirectivesComponent } from './directives/directives.component';
 import { CommonModule } from '@angular/common';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
+    RouterLink,
     BlockComponent,
     UserProfileComponent,
     DirectivesComponent,
+    MenuComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Angular_Quest_03';
+  title = 'Angular_Quest_04';
 }
