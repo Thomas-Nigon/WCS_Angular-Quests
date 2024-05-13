000c9ecbaccf7e7a0e1e6d6a80c5c59997c340f4
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2024-05-13 09:20:28 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { BlockComponent } from './block/block.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { DirectivesComponent } from './directives/directives.component';
 import { CommonModule } from '@angular/common';
+import { MyFormComponent } from './my-form/my-form.component';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,7 @@ import { CommonModule } from '@angular/common';
     BlockComponent,
     UserProfileComponent,
     DirectivesComponent,
+    MyFormComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ src/app/my-form/my-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-form.component.html',
+  styleUrl: './my-form.component.scss'
+})
+export class MyFormComponent {
+
+}
